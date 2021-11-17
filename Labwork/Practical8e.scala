9ee1f2f0736b1edfd133d1ef9eb58636f4b40c1f
TusharPantt <89899791+TusharPantt@users.noreply.github.com>
2021-11-17 13:56:35 +0530

    Create Practical8e.scala

---
+++ Labwork/Practical8e.scala
@@ -0,0 +1,15 @@
+object Scala_8
+{ 
+  
+    // Main method
+    def main(args:Array[String])
+    {
+      
+        // Applying substring method
+        val result = "TusharPant".substring(8)
+          
+        // Displays output
+        println(result)
+      
+    }
+} 
